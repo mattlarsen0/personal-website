f069756bad8cd774993ba6a3ba57facf67723611
Matt Larsen <matt.larsen0@gmail.com>
2026-05-20 12:30:17 -0700

    svg version of gradientbar wip

--- components/livery/gradientBar.tsx
+++ components/livery/gradientBar.tsx
@@ -1,75 +1,47 @@
-import { View, ViewStyle, useWindowDimensions } from 'react-native'
-import useStyles from '@/hooks/styles/useStyles';
+import { Dimensions, DimensionValue, Image, View } from 'react-native'
 
 type GradientBarProps = {
     colors: string[]
 };
+const dimensions = Dimensions.get('window');
 
 export default function GradientBar(props: GradientBarProps) {
-    const styles = useStyles();
-    const dimensions = useWindowDimensions();
-    const barWidth = Math.min(dimensions.width / 20, 100);
-    const barPadding = 50;
-    const sharedStyles: ViewStyle = {
-        borderWidth: 2,
-        borderColor: '#000000',
-        width: barWidth,
-        position: 'relative',
-    };
-
     const bars = props.colors.map((color, index) => {
-        const left = (barWidth * index) + (barPadding * index);
-        const angledBarHeight = index * Math.min(dimensions.width / 10, 100);
-        const barContainer: ViewStyle = {
-            flexDirection: 'row',
-            position: 'absolute',
-            top: 0,
-            left: 0
-        };
-
+        const leftOffset = `${10 * index}%` as DimensionValue;
         return (
-            <View key={`gradientBar-${color}-${index}`}>
-                <View style={barContainer}>
-                    <View style={{
-                        ...sharedStyles,
-                        backgroundColor: color,
-                        transform: [{
-                            rotate: '-45deg'
-                        }],
-                        borderWidth: 1,
-                        borderColor: styles.titleText.color,
-                        height: angledBarHeight,
-                        left: left,
-                        top: 0
-                    }}/>
-                </View>
-                <View style={barContainer}>
-                    <View style={{
-                        ...sharedStyles,
-                        backgroundColor: color,
-                        height: dimensions.height,
-                        transform: [{
-                            rotate: '0deg'
-                        }],
-                        left: left,
-                        top: angledBarHeight/2
-                    }}/>
-                </View>
+            <View style={{
+                zIndex: 0,
+                left: leftOffset,
+                top: 0,
+                height: '100%',
+                width: dimensions.width/4,
+                position: 'absolute'
+            }} key={`gradientBar-${color}-${index}`}>
+                <Image
+                    source={require("@/assets/images/gradient.png")}
+                    style={{
+                        tintColor: color,
+                        resizeMode: "contain",
+                        width: '100%',
+                        aspectRatio: 1,
+                        flex: 1,
+                        left: leftOffset,
+                        top: 0,
+                    }}
+                />
             </View>
         );
-
     })
     return (
         <View style={{
             position: 'absolute',
-            height: '100%',
             top: 0,
             left: 0,
-            right: 0,
-            bottom: 0,
-            zIndex: 1
+            zIndex: 1,
+            height: dimensions.height,
+            width: '100%',
         }}>
-           {bars} 
+            {bars}
         </View>
     );
 }
