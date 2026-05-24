914a3f6178d361039e2970f6bd364f3aca8e9408
Matt <matt.larsen0@gmail.com>
2026-05-24 15:57:13 -0700

    Added latest android build

--- metro.config.cjs
+++ metro.config.cjs
@@ -1,4 +1,4 @@
-const { getDefaultConfig } = require("@expo/metro-config");
+const { getDefaultConfig } = require("expo/metro-config");
 
 // eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
